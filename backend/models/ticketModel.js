4caf8507c031ede4f201740d66b448ab38395719
Joseph Elias <jrelias@outlook.com>
2022-03-24 13:26:09 -0400

    fixed ticket model and added ability for a user to create and find tickets

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -12,7 +12,7 @@ const ticketSchema = mongoose.Schema(
       required: [true, 'Please select a product'],
       enum: ['iPhone', 'Macbook Pro', 'iMac', 'iPad'],
     },
-    password: {
+    description: {
       type: String,
       required: [true, 'Please enter a description of the issue'],
     },
