0743540baaf92f636bc680b0be68503fa609d7f8
Joseph Elias <jrelias@outlook.com>
2022-03-24 14:31:10 -0400

    created custom hook to check auth status, created a new ticket route and page that is protected by a private route that checks the auth status in order to continue

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+// This below lets us select the user from Redux to see if we're logged in
+import { useSelector } from 'react-redux';
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  //get user from state
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+    setCheckingStatus(false);
+  }, [user]);
+
+  return { loggedIn, checkingStatus };
+};
