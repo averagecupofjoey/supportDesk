c579699007a4e9477516ecf0bc64497375b45ae0
Joseph Elias <jrelias@outlook.com>
2022-03-24 17:09:04 -0400

    fetching tickets from backend and adding to store

---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { getTickets, reset } from "../features/tickets/ticketSlice"
+import Spinner from "../components/Spinner"
+import BackButton from "../components/BackButton"
+
+
+function Tickets() {
+  const { tickets, isLoading, isSuccess} = useSelector((state)=>state.tickets)
+
+  const dispatch = useDispatch()
+
+  // clears state on unmount
+  useEffect(()=>{
+    return () => {
+      if(isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  useEffect(()=>{
+    dispatch(getTickets())
+  }, [dispatch])
+
+  if(isLoading){
+    return <Spinner />
+  }
+
+  return (
+    <div>Tickets</div>
+  )
+}
+
+export default Tickets
